183ae710b83efd9e6c12d1e7b3b6378223bf9511
stephen <sburrisjr@gmail.com>
2020-02-08 14:29:09 -0500

    Cleaned up entrypoint.sh.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,10 @@
 #!/bin/sh
 set -e
 
-args="--regex --json --max_depth=500" # Default trufflehog options
+mkdir -p $INPUT_REPORTLOCATION
 
-if [ -n "${INPUT_SCANARGUMENTS}" ]; then
-  args="${INPUT_SCANARGUMENTS}" # Overwrite if new options string is provided
-fi
+java -jar /tmp/oasd.jar $GITHUB_WORKSPACE -json -output-file=$INPUT_REPORTLOCATION/RawOutput.json
+jq . $INPUT_REPORTLOCATION/RawOutput.json > $INPUT_REPORTLOCATION/FormatedOutput.json
 
-mkdir -p /$GITHUB_WORKSPACE/Reports
-
-java -jar /tmp/oasd.jar $GITHUB_WORKSPACE -json -output-file=$GITHUB_WORKSPACE/Reports/RawOutput.json
-jq . $GITHUB_WORKSPACE/Reports/RawOutput.json > $GITHUB_WORKSPACE/Reports/FormatedOutput.json
-
-java -jar /tmp/oasd.jar $GITHUB_WORKSPACE -full-json -output-file=$GITHUB_WORKSPACE/Reports/FullJsonOutput.json
-jq . $GITHUB_WORKSPACE/Reports/FullJsonOutput.json > $GITHUB_WORKSPACE/Reports/FormatedFullJsonOutput.json
+java -jar /tmp/oasd.jar $GITHUB_WORKSPACE -full-json -output-file=$INPUT_REPORTLOCATION/FullJsonOutput.json
+jq . $INPUT_REPORTLOCATION/FullJsonOutput.json > $INPUT_REPORTLOCATION/FormatedFullJsonOutput.json
